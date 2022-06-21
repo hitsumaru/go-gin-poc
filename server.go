b87bbda431748670f57eefcd961840985448dd7a
hitsumaru <vmsilva19@gmail.com>
2022-06-21 02:36:51 -0300

    Fix method setupLogOutput \ add gindump to server

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -16,15 +17,16 @@ var (
 )
 
 func setupLogOutput() {
-	f, _: os.Create("gin.log")
-	gin.DefaultWriter := io.MultiWriter(f, os.Stdout)
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }
 
 func main() {
 	setupLogOutput()
 	server := gin.New()
 
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
+	server.Use(gin.Recovery(), middlewares.Logger(),
+		middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
