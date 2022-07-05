58c64427280bec19cb0d74fb46a12f4ad20d7270
hitsumaru <vmsilva19@gmail.com>
2022-07-05 00:49:22 -0300

    Add data binding and validation implementation

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -3,12 +3,14 @@ package controller
 import (
 	"go-gin-poc/entity"
 	"go-gin-poc/service"
+	"go-gin-poc/validators"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type VideoController interface {
-	Save(ctx *gin.Context) entity.Video
+	Save(ctx *gin.Context) error
 	FindAll() []entity.Video
 }
 
@@ -16,7 +18,11 @@ type controller struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
 	return &controller{
 		service: service,
 	}
@@ -26,9 +32,16 @@ func (c *controller) FindAll() []entity.Video {
 	return c.service.FindAll()
 }
 
-func (c *controller) Save(ctx *gin.Context) entity.Video {
+func (c *controller) Save(ctx *gin.Context) error {
 	var video entity.Video
-	ctx.BindJSON(&video)
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
+	if err != nil {
+		return err
+	}
 	c.service.Save(video)
-	return video
+	return nil
 }
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"go-gin-poc/middlewares"
 	"go-gin-poc/service"
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -25,15 +26,21 @@ func main() {
 	setupLogOutput()
 	server := gin.New()
 
+	// for test purposes middlewares.BasicAuth()
 	server.Use(gin.Recovery(), middlewares.Logger(),
-		middlewares.BasicAuth(), gindump.Dump())
+		gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
 
 	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is Valid!!"})
+		}
 	})
 
 	server.Run(":8080")
