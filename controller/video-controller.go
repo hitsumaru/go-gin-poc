a3a8c01b0a05566a4ec37c85f6fd344336347654
hitsumaru <vmsilva19@gmail.com>
2022-07-08 01:14:33 -0300

    Add and implemented ShowAll

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -4,6 +4,7 @@ import (
 	"go-gin-poc/entity"
 	"go-gin-poc/service"
 	"go-gin-poc/validators"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -12,6 +13,7 @@ import (
 type VideoController interface {
 	Save(ctx *gin.Context) error
 	FindAll() []entity.Video
+	ShowAll(ctx *gin.Context)
 }
 
 type controller struct {
@@ -44,4 +46,13 @@ func (c *controller) Save(ctx *gin.Context) error {
 	}
 	c.service.Save(video)
 	return nil
+}
+
+func (c *controller) ShowAll(ctx *gin.Context) {
+	videos := c.service.FindAll()
+	data := gin.H{
+		"title":  "Video Page",
+		"videos": videos,
+	}
+	ctx.HTML(http.StatusOK, "index.html", data)
 }
